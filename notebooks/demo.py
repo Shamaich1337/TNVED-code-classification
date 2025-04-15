9b01b7cffcaf1a3c45cf58842775d0707af3bef4
Shamaich <ah.shoma159@gmail.com>
2025-04-15 23:27:03 +0300

    added demo

--- notebooks/demo.py
+++ notebooks/demo.py
@@ -23,19 +23,19 @@ def predict_tnved_code(text):
     :param text: str - входной текст для классификации
     :return: int - предсказанный TNVED-код
     """
-    # Токенизируем текст
+    
     inputs = tokenizer(
         text,
-        return_tensors="pt",  # Возвращаем тензоры PyTorch
-        truncation=True,      # Обрезаем текст до максимальной длины
-        padding=True,         # Дополняем текст до максимальной длины
-        max_length=128        # Максимальная длина последовательности
+        return_tensors="pt",  
+        truncation=True,      
+        padding=True,         
+        max_length=128        
     )
     
-    # Перемещаем данные на устройство (GPU или CPU)
+    
     inputs = {key: val.to(device) for key, val in inputs.items()}
     
-    # Выполняем предсказание
+    
     model.eval()  
     with torch.no_grad():
         outputs = model(**inputs)
@@ -51,6 +51,6 @@ if __name__ == "__main__":
     
     input_text = "The ADMV1014 is a silicon germanium (SiGe) wideband microwave downconverter designed for point-to-point microwave radio applications. It operates within a frequency range of 24 GHz to 44 GHz and provides two modes of frequency translation: direct quadrature demodulation to baseband I/Q output signals and image rejection downconversion to a complex intermediate frequency (IF) output. The device is programmable via a 4-wire SPI interface and is housed in a compact 32-terminal, 5 mm × 5 mm Land Grid Array (LGA) package."
     
-    # Получаем предсказанный TNVED-код
+   
     predicted_code = predict_tnved_code(input_text)
     print(f"Предсказанный TNVED-код: {predicted_code}")
